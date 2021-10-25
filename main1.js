2439081fd89119b99de9379fe42e76aba6d36f1e
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-25 16:51:29 +0300

    good job suha

--- main1.js
+++ main1.js
@@ -1,12 +1,38 @@
 // {client:"website4",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
 const portfoliWorks = JSON.parse(localStorage.getItem("portfolio"));
-console.log(portfoliWorks);
 
+
+const randerWork= (index) =>{
+    console.log(portfoliWorks[index].client);
+    $("#home").hide();
+    // $('#discPage').addClass("show");
+    $('#discPage').append(
+        `<div class="disc">
+
+        <div class="discContent">
+        <h3>Client: <span>${portfoliWorks[index].client}</span></h3>
+        <h3>Created: <span>${portfoliWorks[index].created}</span></h3>
+        <h3>Teachnologies: <span>${portfoliWorks[index].tech}</span></h3>
+        <a href="" >
+        <span style="color: red;"class="fa fa-heart" aria-hidden="true"></span>
+        </a>
+        </div>
+
+        <div class="grid" >
+        <img class="discImg" src="${portfoliWorks[index].imgUrl}" alt="">
+        <img class="discImg" src="${portfoliWorks[index].imgUrl2}" alt="">
+        <img class="discImg" src="${portfoliWorks[index].imgUrl3}" alt="">
+        </div>
+
+    </div>`
+      );};
+
+const randerPortfolio = ()=> {
 for (let i =0 ; i<portfoliWorks.length;i++){
    
     // console.log( portfoliWorks[i].client);
     $('#show').append(`
-    <div class="grid-item">
+    <div class="grid-item" id= "work${i}" >
 <div>
   <img src="${portfoliWorks[i].imgUrl}" alt="">
 </div>
@@ -18,4 +44,11 @@ for (let i =0 ; i<portfoliWorks.length;i++){
 </div> 
     
     `);
+
+    $(`#work${i}`).click(()=>randerWork(i));
+    //loop end
 };
+
+};
+
+randerPortfolio();
