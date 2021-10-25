e8f0648cb041ebe92983ebaa92b892f284368dd5
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-25 16:29:59 +0300

    portfilios

--- main1.js
+++ main1.js
@@ -1,15 +1,21 @@
+// {client:"website4",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
 const portfoliWorks = JSON.parse(localStorage.getItem("portfolio"));
-console.log(portfoliWorks.length);
+console.log(portfoliWorks);
 
 for (let i =0 ; i<portfoliWorks.length;i++){
-    $('#show').append("hi");
+   
+    // console.log( portfoliWorks[i].client);
+    $('#show').append(`
+    <div class="grid-item">
+<div>
+  <img src="${portfoliWorks[i].imgUrl}" alt="">
+</div>
+
+<div>
+  <h2>${portfoliWorks[i].client}</h2>
+</div>
+
+</div> 
+    
+    `);
 };
-// portfoliWorks.forEach((elem,index)=>{
-//     // $('#workPage').addClass("showWork");
-//     $('#show').append(
-//         `<div style="padding-top: 500px;">
-//         <h3>Client: <span>${elem.client}</span></h3>
-//     </div>`
-//       );
-// });
- // <img class="discImg" src="${elem.imgUrl}" alt="">
