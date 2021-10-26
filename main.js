e3107cba0634befa0ee1c032146d969f93eca319
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-26 16:52:59 +0300

    style

--- main.js
+++ main.js
@@ -1,107 +1,146 @@
-//  Header Anmation 
-$(document).ready(function(){
-
-    $("#imgBannerDiv").animate({
-        // bottom: '250px',
-        // height: '+=150px',
-        // width: '+=150px',
-        marginTop:'0px' ,
-        transitionDuration: '2s'
-      });
-      $("#contentBanner").animate({
-        // bottom: '250px',
-        // height: '+=150px',
-        // width: '+=150px',
-        marginTop:'300px' ,
-        transitionDuration: '2s'
-      });
+//  Header Anmation
+$("#home").removeClass("home");
+$(document).ready(function () {
+  $("#imgBannerDiv").animate({
+    // bottom: '250px',
+    // height: '+=150px',
+    // width: '+=150px',
+    marginTop: "0px",
+    transitionDuration: "2s",
+  });
+  $("#contentBanner").animate({
+    // bottom: '250px',
+    // height: '+=150px',
+    // width: '+=150px',
+    marginTop: "300px",
+    transitionDuration: "2s",
+  });
 });
 
 //Informations
-const works =[
- {client:"website0",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
- {client:"website1",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
- {client:"website2",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
- {client:"website3",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
- {client:"website4",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
-
+const works = [
+  {
+    client: "website0",
+    tech: `JS`,
+    created: "2020",
+    imgUrl: `/img1.jpg`,
+    imgUrl2: `/img1.jpg`,
+    imgUrl3: `/img1.jpg`,
+    disc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa, itaque consequatur earum commodi aliquam accusantium tenetur inventore reiciendis alias! Dolores id iure mollitia nemo ad quae possimus expedita iste?`,
+  },
+  {
+    client: "website0",
+    tech: `JS`,
+    created: "2020",
+    imgUrl: `/img1.jpg`,
+    imgUrl2: `/img1.jpg`,
+    imgUrl3: `/img1.jpg`,
+    disc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa, itaque consequatur earum commodi aliquam accusantium tenetur inventore reiciendis alias! Dolores id iure mollitia nemo ad quae possimus expedita iste?`,
+  },
+  {
+    client: "website0",
+    tech: `JS`,
+    created: "2020",
+    imgUrl: `/img1.jpg`,
+    imgUrl2: `/img1.jpg`,
+    imgUrl3: `/img1.jpg`,
+    disc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa, itaque consequatur earum commodi aliquam accusantium tenetur inventore reiciendis alias! Dolores id iure mollitia nemo ad quae possimus expedita iste?`,
+  },
+  {
+    client: "website0",
+    tech: `JS`,
+    created: "2020",
+    imgUrl: `/img1.jpg`,
+    imgUrl2: `/img1.jpg`,
+    imgUrl3: `/img1.jpg`,
+    disc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa, itaque consequatur earum commodi aliquam accusantium tenetur inventore reiciendis alias! Dolores id iure mollitia nemo ad quae possimus expedita iste?`,
+  },
+  {
+    client: "website0",
+    tech: `JS`,
+    created: "2020",
+    imgUrl: `/img1.jpg`,
+    imgUrl2: `/img1.jpg`,
+    imgUrl3: `/img1.jpg`,
+    disc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa, itaque consequatur earum commodi aliquam accusantium tenetur inventore reiciendis alias! Dolores id iure mollitia nemo ad quae possimus expedita iste?`,
+  },
 ];
 
-
-
 //rander of single page discription
-const randerWork= (index) =>{
-    $("#home").hide();
-    $('#workPage').addClass("showWork");
-    $('#workPage').append(
-        `    <div class="disc">
-        <div class="discContent">
-        <h3>Client: <span>${works[index].client}</span></h3>
-        <h3>Created: <span>${works[index].created}</span></h3>
-        <h3>Teachnologies: <span>${works[index].tech}</span></h3>
-        <a href="" ><span style="color: red;"class="fa fa-heart" aria-hidden="true">
-        </span></a>
-        </div>
-        <div class="grid" >
-        <img class="discImg" src="${works[index].imgUrl}" alt="">
-        <img class="discImg" src="${works[index].imgUrl2}" alt="">
-        <img class="discImg" src="${works[index].imgUrl3}" alt="">
-        </div>
+const randerWork = (index) => {
+  $("#home").hide();
+  $("#workPage").addClass("showWork");
+  // $('#workPage').append(
+  //     `    <div class="disc">
+  //     <div class="discContent">
+  //     <h3>Client: <span>${works[index].client}</span></h3>
+  //     <h3>Created: <span>${works[index].created}</span></h3>
+  //     <h3>Teachnologies: <span>${works[index].tech}</span></h3>
+  //     <a href="" ><span style="color: red;"class="fa fa-heart" aria-hidden="true">
+  //     </span></a>
+  //     </div>
+  //     <div class="grid" >
+  //     <img class="discImg" src="${works[index].imgUrl}" alt="">
+  //     <img class="discImg" src="${works[index].imgUrl2}" alt="">
+  //     <img class="discImg" src="${works[index].imgUrl3}" alt="">
+  //     </div>
 
-    </div>`
-      );
-//end of forEach
+  // </div>`
+  $("#workPage").append(`
+
+<div class="discContainer">
+<div class="blue "></div>
+<div class="purple"></div>
+<div class="white">
+    <div class="t">
+    <h1>${works[index].client}</h1>
+    <p>${works[index].disc}</p></div>
+    </div>
+    <div class="dImg">
+    <img src="${works[index].imgUrl2}" alt="image" class="singlePageImg">
+    s</div>
+</div>`);
+  //end of forEach
 };
-  
+
 //rander of partfilio
-const worksRander = ()=>{
-    $("#home").hide();
-    $('#workPage').addClass("showWork");
+const worksRander = () => {
+  $("#home").hide();
+  $("#workPage").addClass("showWork");
 
-    works.forEach((elem, index) => { 
-      $("#workPage").append(
-        `<div class="card" id="card${index}"><img src="${elem.imgUrl}"" alt="" id="imgCard${index}"/></div>`
-      );
-      
-      //end of loop
-    });
+  works.forEach((elem, index) => {
+    $("#workPage").append(
+      `<div class="card" id="card${index}"><img src="${elem.imgUrl}"" alt="" id="imgCard${index}"/></div>`
+    );
 
-  };
+    //end of loop
+  });
+};
 //   $("#portfolio").click(worksRander);
-
 
 //rander recent 4 work in home
 const randerWorks = () => {
+  // works.forEach((elem, index) => {
+  for (let index = 0; index < 4; index++) {
+    // $("#cards").append(
+    //   //  `<h1>hi</h1>`
+    //   `<div class="grid-item" id="card${index}"><img src="${works[index].imgUrl}"" alt="" id="imgCard${index}"/></div>`
+    // );
+    $(`#card${index}`).click(() => randerWork(index));
 
-    // works.forEach((elem, index) => {
-        for(let index=0; index<5; index++){
-
-      $("#cards").append(
-        //  `<h1>hi</h1>`
-        `<div class="grid-item" id="card${index}"><img src="${works[index].imgUrl}"" alt="" id="imgCard${index}"/></div>`
-      );
-      $(`#card${index}`).click(()=>randerWork(index));
-
-    
-
-   
-      //end of loop
+    //end of loop
     // });
-      };
-  };
+  }
+};
 
-
- //rander of Portfolio page
+//rander of Portfolio page
 const randerPortfolio = () => {
-   
-works.forEach((elem,index)=>{
-
-  localStorage.setItem("portfolio",JSON.stringify(works));
-  location.replace("portfolio.html");
-
-});
-      //end of loop
-  };
-$('#portfolioLink').click(randerPortfolio);
+  works.forEach((elem, index) => {
+    localStorage.setItem("portfolio", JSON.stringify(works));
+    location.replace("portfolio.html");
+  });
+  //end of loop
+};
+$("#portfolioLink").click(randerPortfolio);
 
 randerWorks();
--- main2.js
+++ main2.js
@@ -1,4 +1,4 @@
-// {client:"website4",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`},
+// {client:"website4",tech:`JS`, created: "2020" , imgUrl:`/img1.jpg`,imgUrl2:`/img1.jpg`,imgUrl3:`/img1.jpg`,disc:},
 const portfoliWorks = JSON.parse(localStorage.getItem("portfolio"));
 
 
@@ -7,25 +7,38 @@ const randerWork= (index) =>{
     $("#home").hide();
     // $('#discPage').addClass("show");
     $('#discPage').append(
-        `<div class="disc">
+    //     `<div class="disc">
 
-        <div class="discContent">
-        <h3>Client: <span>${portfoliWorks[index].client}</span></h3>
-        <h3>Created: <span>${portfoliWorks[index].created}</span></h3>
-        <h3>Teachnologies: <span>${portfoliWorks[index].tech}</span></h3>
-        <a href="" >
-        <span style="color: red;"class="fa fa-heart" aria-hidden="true"></span>
-        </a>
-        </div>
+    //     <div class="discContent">
+    //     <h3>Client: <span>${portfoliWorks[index].client}</span></h3>
+    //     <h3>Created: <span>${portfoliWorks[index].created}</span></h3>
+    //     <h3>Teachnologies: <span>${portfoliWorks[index].tech}</span></h3>
+    //     <a href="" >
+    //     <span style="color: red;"class="fa fa-heart" aria-hidden="true"></span>
+    //     </a>
+    //     </div>
 
-        <div class="grid" >
-        <img class="discImg" src="${portfoliWorks[index].imgUrl}" alt="">
-        <img class="discImg" src="${portfoliWorks[index].imgUrl2}" alt="">
-        <img class="discImg" src="${portfoliWorks[index].imgUrl3}" alt="">
-        </div>
+    //     <div class="grid" >
+    //     <img class="discImg" src="${portfoliWorks[index].imgUrl}" alt="">
+    //     <img class="discImg" src="${portfoliWorks[index].imgUrl2}" alt="">
+    //     <img class="discImg" src="${portfoliWorks[index].imgUrl3}" alt="">
+    //     </div>
 
-    </div>`
-      );};
+    // </div>`
+    `<div class="discContainer">
+<div class="blue "></div>
+<div class="purple"></div>
+<div class="white">
+    <div class="t">
+    <h1>${portfoliWorks[index].client}</h1>
+    <p>${portfoliWorks[index].disc}</p></div>
+    </div>
+    <div class="dImg"> <img src="${portfoliWorks[index].imgUrl2}" alt="image" class="singlePageImg"></div>
+</div>`
+      );
+    
+    
+    };
 
 const randerPortfolio = ()=> {
 for (let i =0 ; i<portfoliWorks.length;i++){
@@ -36,14 +49,12 @@ for (let i =0 ; i<portfoliWorks.length;i++){
 <div>
   <img src="${portfoliWorks[i].imgUrl}" alt="">
 </div>
-
 <div>
   <h2>${portfoliWorks[i].client}</h2>
 </div>
-
 </div> 
-    
     `);
+    
 
     $(`#work${i}`).click(()=>randerWork(i));
     //loop end
